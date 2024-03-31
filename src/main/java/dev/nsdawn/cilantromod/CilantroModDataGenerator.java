032a5e7acfc129d6dfd117ba64ccc0655f4a7146
NSDawn <tnahsin.airus@gmail.com>
2024-03-31 08:13:56 -0700

    Initial structure commit

---
+++ src/main/java/dev/nsdawn/cilantromod/CilantroModDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.nsdawn.cilantromod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CilantroModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
