84ac0e3b3a8daad7cd960c137452597147354e52
NSDawn <tnahsin.airus@gmail.com>
2024-03-31 10:41:19 -0700

    Implementing Items: coriander_seeds, cilantro

---
+++ src/main/java/dev/nsdawn/cilantromod/CilantroModClient.java
@@ -0,0 +1,10 @@
+package dev.nsdawn.cilantromod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CilantroModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
