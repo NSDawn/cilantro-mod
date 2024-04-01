9f8de59a8e2b5d85c0d7a976a87794852075c492
NSDawn <tnahsin.airus@gmail.com>
2024-03-31 22:02:31 -0700

    readme start

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cilantro-mod
-A Minecraft mod that adds Cilantro to the game, so I can learn how to use Fabric to make mods :)
+A Minecraft mod that adds Cilantro to the game, using the Fabric API.
+## Specs
+- Requires Fabric API
+- For Minecraft 1.20.x
+## Features
+### Cilantro
+![Cilantro Sprite](/public-assets/cilantro.png)
+
--- src/main/java/dev/nsdawn/cilantromod/datagen/ModLootTableProvider.java
+++ src/main/java/dev/nsdawn/cilantromod/datagen/ModLootTableProvider.java
@@ -5,17 +5,12 @@ import dev.nsdawn.cilantromod.block.blocks.CilantroCropBlock;
 import dev.nsdawn.cilantromod.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
-import net.minecraft.block.Blocks;
-import net.minecraft.data.server.loottable.BlockLootTableGenerator;
-import net.minecraft.data.server.loottable.vanilla.VanillaBlockLootTableGenerator;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LeafEntry;
-import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
 import net.minecraft.predicate.StatePredicate;
 
@@ -28,6 +23,7 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
+
         //VanillaBlockLootTableGenerator
         BlockStatePropertyLootCondition.Builder cilantroBuilderState6 = BlockStatePropertyLootCondition.builder(ModBlocks.CILANTRO_CROP).properties(StatePredicate.Builder.create().exactMatch(CilantroCropBlock.AGE, 6));
         BlockStatePropertyLootCondition.Builder cilantroBuilderState5 = BlockStatePropertyLootCondition.builder(ModBlocks.CILANTRO_CROP).properties(StatePredicate.Builder.create().exactMatch(CilantroCropBlock.AGE, 5));
@@ -40,16 +36,16 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.CILANTRO_CROP, LootTable.builder()
                 .pool(LootPool.builder().conditionally(cilantroBuilderState6).with(ItemEntry.builder(Items.STICK)))
                 .pool(LootPool.builder().conditionally(cilantroBuilderState6).with(ItemEntry.builder(ModItems.CORIANDER_SEEDS)))
-                .pool(LootPool.builder().conditionally(cilantroBuilderState6).with((LootPoolEntry.Builder<?>)(ItemEntry.builder(ModItems.CORIANDER_SEEDS).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 3)))))
+                .pool(LootPool.builder().conditionally(cilantroBuilderState6).with((ItemEntry.builder(ModItems.CORIANDER_SEEDS).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 3)))))
                 .pool(LootPool.builder().conditionally(cilantroBuilderState5).with(ItemEntry.builder(Items.STICK)))
-                .pool(LootPool.builder().conditionally(cilantroBuilderState5).with((LootPoolEntry.Builder<?>)(ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 1)))))
-                .pool(LootPool.builder().conditionally(cilantroBuilderState4).with((LootPoolEntry.Builder<?>)(ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 4)))))
+                .pool(LootPool.builder().conditionally(cilantroBuilderState5).with((ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 1)))))
+                .pool(LootPool.builder().conditionally(cilantroBuilderState4).with((ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 4)))))
                 .pool(LootPool.builder().conditionally(cilantroBuilderState4).with(ItemEntry.builder(ModItems.CILANTRO)))
-                .pool(LootPool.builder().conditionally(cilantroBuilderState3).with((LootPoolEntry.Builder<?>)(ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 3)))))
+                .pool(LootPool.builder().conditionally(cilantroBuilderState3).with((ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 3)))))
                 .pool(LootPool.builder().conditionally(cilantroBuilderState3).with(ItemEntry.builder(ModItems.CILANTRO)))
-                .pool(LootPool.builder().conditionally(cilantroBuilderState2).with((LootPoolEntry.Builder<?>)(ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 1)))))
+                .pool(LootPool.builder().conditionally(cilantroBuilderState2).with((ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 1)))))
                 .pool(LootPool.builder().conditionally(cilantroBuilderState2).with(ItemEntry.builder(ModItems.CILANTRO)))
-                .pool(LootPool.builder().conditionally(cilantroBuilderState1).with((LootPoolEntry.Builder<?>)(ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 1)))))
+                .pool(LootPool.builder().conditionally(cilantroBuilderState1).with((ItemEntry.builder(ModItems.CILANTRO).apply(ApplyBonusLootFunction.binomialWithBonusCount(Enchantments.FORTUNE, 0.5714286f, 1)))))
                 .pool(LootPool.builder().conditionally(cilantroBuilderState0).with(ItemEntry.builder(ModItems.CORIANDER_SEEDS)))
         );
 
